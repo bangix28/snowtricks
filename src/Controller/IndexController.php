af6d465d955b464ffbe78fc8dc1b2ff0a29cd7fe
granger kenolane <kenolane28@gmail.com>
2020-11-09 16:34:43 +0100

    Add paginator to controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\Paginator;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(Request $request,PaginatorInterface $paginator): Response
     {
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'tricks' => $paginator->paginate(
+                $this->entityManager->getRepository('App:Post')->findAll(),
+                $request->query->getInt('page',1),
+                15
+            ),
         ]);
     }
 }
